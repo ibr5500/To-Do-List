4959ac534026bed806031c89a68448b0833bb411
unknown <ibr.s.ahmat@gmail.com>
2022-04-30 07:25:49 +0300

    Fixed linters errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import Task from './moudles/task.js';
 import './index.css';
 
-
 let tasksList = JSON.parse(localStorage.getItem('tasks')) || [];
 
 const tasks = document.querySelector('.task-items');
 const displayTask = () => {
-    tasks.innerHTML = tasksList.map((task) => `
+  tasks.innerHTML = tasksList.map((task) => `
       <div id="${task.index}" class="task">
         <div>
             <input id="${task.index}" class="checkbox" type="checkbox" name="checkbox" ${!task.completed ? '' : 'checked'} />
@@ -19,63 +18,63 @@ const displayTask = () => {
 };
 
 const deleteTask = (e) => {
-    const item = e.target;
-    if (item.classList.contains('fa-trash')) {
-        const removeParent = item.parentElement;
-        removeParent.remove();
-        const newTaskList = tasksList.filter((elem) => +elem.index !== +removeParent.id);
-        const updateTaskList = newTaskList.map((elem, index) => {
-            elem.index = index + 1;
-            return elem;
-        });
-        localStorage.setItem('tasks', JSON.stringify(updateTaskList));
-        tasksList = updateTaskList;
-        displayTask();
-    }
+  const item = e.target;
+  if (item.classList.contains('fa-trash')) {
+    const removeParent = item.parentElement;
+    removeParent.remove();
+    const newTaskList = tasksList.filter((elem) => +elem.index !== +removeParent.id);
+    const updateTaskList = newTaskList.map((elem, index) => {
+      elem.index = index + 1;
+      return elem;
+    });
+    localStorage.setItem('tasks', JSON.stringify(updateTaskList));
+    tasksList = updateTaskList;
+    displayTask();
+  }
 };
 tasks.addEventListener('click', deleteTask);
 
 tasks.addEventListener('keypress', (event) => {
-    if (event.target.type === 'text' && event.key === 'Enter') {
-        const targetedElem = event.target.parentElement.parentElement;
-        tasksList.filter((e) => +e.index === +targetedElem.id);
-        tasksList[targetedElem.id - 1].description = event.target.value;
-        localStorage.setItem('tasks', JSON.stringify(tasksList));
-    }
+  if (event.target.type === 'text' && event.key === 'Enter') {
+    const targetedElem = event.target.parentElement.parentElement;
+    tasksList.filter((e) => +e.index === +targetedElem.id);
+    tasksList[targetedElem.id - 1].description = event.target.value;
+    localStorage.setItem('tasks', JSON.stringify(tasksList));
+  }
 });
 
 tasks.addEventListener('change', (event) => {
-    if (event.target.checked) {
-        event.target.nextElementSibling.classList.add('checked');
-        const index = event.target.id;
-        tasksList[index - 1].completed = true;
-        localStorage.setItem('tasks', JSON.stringify(tasksList));
-        displayTask();
-    } else {
-        event.target.nextElementSibling.classList.remove('checked');
-        const index = event.target.id;
-        tasksList[index - 1].completed = false;
-        localStorage.setItem('tasks', JSON.stringify(tasksList));
-        displayTask();
-    }
+  if (event.target.checked) {
+    event.target.nextElementSibling.classList.add('checked');
+    const index = event.target.id;
+    tasksList[index - 1].completed = true;
+    localStorage.setItem('tasks', JSON.stringify(tasksList));
+    displayTask();
+  } else {
+    event.target.nextElementSibling.classList.remove('checked');
+    const index = event.target.id;
+    tasksList[index - 1].completed = false;
+    localStorage.setItem('tasks', JSON.stringify(tasksList));
+    displayTask();
+  }
 });
 
 const refresh = document.querySelector('#refersh');
 refresh.addEventListener('click', () => {
-    window.location.reload();
+  window.location.reload();
 });
 
 const clearAll = document.querySelector('#clear');
 clearAll.addEventListener('click', () => {
-    let tasks = [];
-    tasksList.map((task) => {
-        if (!task.completed) {
-            tasks = [...tasks, task];
-        }
-        tasksList = tasks;
-    });
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    displayTask();
+  let tasks = [];
+    tasksList.map((task) => { // eslint-disable-line
+    if (!task.completed) {
+      tasks = [...tasks, task];
+    }
+    tasksList = tasks;
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  displayTask();
 });
 
 const addNewTask = document.querySelector('#new-item');
@@ -83,32 +82,32 @@ const enter = document.querySelector('#enter');
 const enterKey = document.querySelector('#new-item');
 
 const addTask = () => {
-    enter.addEventListener('click', () => {
-        if (!addNewTask.value) return;
-        const index = tasksList.length + 1;
-        const description = addNewTask.value;
-        let completed;
-        tasksList = [...tasksList, new Task(index, description, completed)];
-        localStorage.setItem('tasks', JSON.stringify(tasksList));
-        displayTask();
-        addNewTask.value = '';
-    });
+  enter.addEventListener('click', () => {
+    if (!addNewTask.value) return;
+    const index = tasksList.length + 1;
+    const description = addNewTask.value;
+    let completed;
+    tasksList = [...tasksList, new Task(index, description, completed)];
+    localStorage.setItem('tasks', JSON.stringify(tasksList));
+    displayTask();
+    addNewTask.value = '';
+  });
 
-    enterKey.addEventListener('keyup', (e) => {
-        if (e.keyCode === 13) {
-            if (!addNewTask.value) return;
-            const index = tasksList.length + 1;
-            const description = addNewTask.value;
-            let completed;
-            tasksList = [...tasksList, new Task(index, description, completed)];
-            localStorage.setItem('tasks', JSON.stringify(tasksList));
-            displayTask();
-            addNewTask.value = '';
-        }
-    });
+  enterKey.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+      if (!addNewTask.value) return;
+      const index = tasksList.length + 1;
+      const description = addNewTask.value;
+      let completed;
+      tasksList = [...tasksList, new Task(index, description, completed)];
+      localStorage.setItem('tasks', JSON.stringify(tasksList));
+      displayTask();
+      addNewTask.value = '';
+    }
+  });
 };
 
 window.addEventListener('load', () => {
-    displayTask();
-    addTask();
+  displayTask();
+  addTask();
 });
--- src/moudles/task.js
+++ src/moudles/task.js
@@ -1,7 +1,7 @@
 export default class Task {
-    constructor(index, description, completed = false) {
-        this.index = index;
-        this.description = description;
-        this.completed = completed;
-    }
+  constructor(index, description, completed = false) {
+    this.index = index;
+    this.description = description;
+    this.completed = completed;
+  }
 }
