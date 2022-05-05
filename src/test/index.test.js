76370e26d34d84b80a68e36ffadcda8a3a73d0f9
unknown <ibr.s.ahmat@gmail.com>
2022-05-05 10:35:20 +0300

    Fixed linters errors

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,14 +1,14 @@
-const { addTask } = require("../index.js");
+const { addTask } = require('../index.js');
 
 describe('Test  Add and Delete functions', () => {
-    test('Add a new task', () => {
-        const tasksList = [];
-        addTask(tasksList, 'test the addition of a task', false);
-        expect(tasksList).toEqual([{
-            index: 1,
-            description: 'test the addition of a task',
-            completed: false,
-        }]);
-        // expect(localStorageMock.setItem).toBeCalledWith('tasks', JSON.stringify(tasksList));
-    });
+  test('Add a new task', () => {
+    const tasksList = [];
+    addTask(tasksList, 'test the addition of a task', false);
+    expect(tasksList).toEqual([{
+      index: 1,
+      description: 'test the addition of a task',
+      completed: false,
+    }]);
+    // expect(localStorageMock.setItem).toBeCalledWith('tasks', JSON.stringify(tasksList));
+  });
 });
